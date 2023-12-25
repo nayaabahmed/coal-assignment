adba8da9e50e395ff964886c5edb9509815f16aa
nayaabahmed <154801299+nayaabahmed@users.noreply.github.com>
2023-12-25 05:12:22 -0800

    Add files via upload

---
+++ question2.asm
@@ -0,0 +1,24 @@
+[org 0x0100]
+jmp start
+multiplicand: dd 1300
+multiplier: dw 500
+result: dd 0
+
+start: mov cl,16
+      mov dx,[multiplier]
+
+checkbit: shr dx,1
+          jnc skipbit
+         
+mov ax,[multiplicand]
+add [result],ax
+mov ax,[multiplicand+2]
+adc [result+2],ax
+
+skipbit: shl word[multiplicand],1
+rcl word[multiplicand+2],1
+dec cl
+jnz checkbit
+
+mov ax,0x4c00
+int 0x21
